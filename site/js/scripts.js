c04c9dfb3f06881ff2f0efae8def698c8759e63d
Sandeep Kumar <sandeep.kumar@vocus.com.au>
2018-02-23 19:27:26 +1100

    Cleanup the home page. Include carousel appearance and disappearance when navigating to & from homepage. Change MANAM welcome message

--- site/js/scripts.js
+++ site/js/scripts.js
@@ -4,46 +4,57 @@ $( document ).ready(function() {
     $('#navigation').load('../partials/_navigation.html');
     $('#footer').load('../partials/_footer.html');
     $('#main').load('../partials/_home.html');
+    $('#carousel').load('../partials/_carousel.html');
 });
 
 $( "html").on('click','#about', function() {
     $('#main').load('../partials/about.html');
+    $('#carousel').hide();
 });
 
 $( "html").on('click','#team', function() {
     $('#main').load('../partials/_team.html');
+    $('#carousel').hide();
 });
 
 $( "html").on('click','#blog', function() {
     $('#main').load('../partials/blog.html');
+    $('#carousel').hide();
 });
 
 $( "html").on('click','#events', function() {
     $('#main').load('../partials/events.html');
+    $('#carousel').hide();
 });
 
 $( "html").on('click','#home', function() {
     $('#main').load('../partials/_home.html');
+    $('#carousel').load('../partials/_carousel.html');
 });
 
 $( "html").on('click','#whatsapp', function() {
     $('#main').load('../partials/_whatsapp.html');
+    $('#carousel').hide();
 });
 
 $( "html").on('click','#studentJobRegistration', function() {
     $('#main').load('../partials/_studentJobRegistration.html');
+    $('#carousel').hide();
 });
 
 $( "html").on('click','#travelAssistance', function() {
     $('#main').load('../partials/_travelAssistance.html');
+    $('#carousel').hide();
 });
 
 $( "html").on('click','#testimonials', function() {
     $('#main').load('../partials/_testimonials.html');
+    $('#carousel').hide();
 });
 
 $( "html").on('click','#logo', function() {
     $('#main').load('../partials/_home.html');
+    $('#carousel').load('../partials/_carousel.html');
 });
 
 $( "html").on('click','#sankranti', function() {
