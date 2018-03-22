19fcd2a11a47f358dc6cf3699eb57b26bc6976ff
Sandeep Kumar <sandeep.kumar@vocus.com.au>
2018-03-22 21:10:20 +1100

    Use relative path instead of file path for the partial html files

--- site/js/scripts.js
+++ site/js/scripts.js
@@ -1,69 +1,69 @@
 
 
 $( document ).ready(function() {
-    $('#navigation').load('../partials/_navigation.html');
-    $('#footer').load('../partials/_footer.html');
-    $('#main').load('../partials/_home.html');
-    $('#carousel').load('../partials/_carousel.html');
+    $('#navigation').load('partials/_navigation.html');
+    $('#footer').load('partials/_footer.html');
+    $('#main').load('partials/_home.html');
+    $('#carousel').load('partials/_carousel.html');
 });
 
 $( "html").on('click','#about', function() {
-    $('#main').load('../partials/about.html');
+    $('#main').load('partials/about.html');
     $('#carousel').hide();
 });
 
 $( "html").on('click','#team', function() {
-    $('#main').load('../partials/_team.html');
+    $('#main').load('partials/_team.html');
     $('#carousel').hide();
 });
 
 $( "html").on('click','#blog', function() {
-    $('#main').load('../partials/blog.html');
+    $('#main').load('partials/blog.html');
     $('#carousel').hide();
 });
 
 $( "html").on('click','#events', function() {
-    $('#main').load('../partials/events.html');
+    $('#main').load('partials/events.html');
     $('#carousel').hide();
 });
 
 $( "html").on('click','#home', function() {
-    $('#main').load('../partials/_home.html');
+    $('#main').load('partials/_home.html');
     $('#carousel').load('../partials/_carousel.html');
 });
 
 $( "html").on('click','#whatsapp', function() {
-    $('#main').load('../partials/_whatsapp.html');
+    $('#main').load('partials/_whatsapp.html');
     $('#carousel').hide();
 });
 
 $( "html").on('click','#studentJobRegistration', function() {
-    $('#main').load('../partials/_studentJobRegistration.html');
+    $('#main').load('partials/_studentJobRegistration.html');
     $('#carousel').hide();
 });
 
 $( "html").on('click','#jobAssistanceRegistration', function() {
-    $('#main').load('../partials/_jobAssistanceRegistration.html');
+    $('#main').load('partials/_jobAssistanceRegistration.html');
     $('#carousel').hide();
 });
 
 $( "html").on('click','#travelAssistance', function() {
-    $('#main').load('../partials/_travelAssistance.html');
+    $('#main').load('partials/_travelAssistance.html');
     $('#carousel').hide();
 });
 
 $( "html").on('click','#testimonials', function() {
-    $('#main').load('../partials/_testimonials.html');
+    $('#main').load('partials/_testimonials.html');
     $('#carousel').hide();
 });
 
 $( "html").on('click','#logo', function() {
-    $('#main').load('../partials/_home.html');
-    $('#carousel').load('../partials/_carousel.html');
+    $('#main').load('partials/_home.html');
+    $('#carousel').load('partials/_carousel.html');
 });
 
 $( "html").on('click','#sankranti', function() {
-    $('#main').load('../partials/_sankranti.html');
+    $('#main').load('partials/_sankranti.html');
 });
 
 $(document).on('click','.navbar-collapse.in',function(e) {
